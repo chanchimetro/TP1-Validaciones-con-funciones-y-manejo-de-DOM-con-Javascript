4632332dad8442b22930a18dd781953854a3a4c8
IGNACIO MATIAS LOPEZ ABRAHAM <46872351@ort>
2023-03-16 10:39:21 -0300

    ok

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 
 function validarNota(id) {
     var nota = document.getElementById(id).value;
-    if(nota>=1 && nota<=10) {
+    if (nota >= 1 && nota <= 10) {
         document.getElementById(id).style.color = "lightgreen";
-        
+
     } else {
         document.getElementById(id).style.color = "red";
-        
+
     }
 }
 
 function validarForms() {
     for (let index = 0; index < document.getElementsByTagName("input").length; index++) {
-        if(document.getElementsByTagName("input")[index].value == "" || document.getElementsByTagName("input")[index].value > 10 || document.getElementsByTagName("input")[index].value < 1) {
+        if (document.getElementsByTagName("input")[index].value == "" || document.getElementsByTagName("input")[index].value > 10 || document.getElementsByTagName("input")[index].value < 1) {
             alert("Porfavor complete todos los campos copn valores válidos (1 a 10).");
             return false;
         }
@@ -21,23 +21,23 @@ function validarForms() {
 }
 
 function imprimirNotas() {
-    var liMate = document.getElementById("limatematica").innerHTML;
-    document.getElementById("limatematica").innerHTML = liMate + document.getElementById("matematica").value;
-    var liLengua = document.getElementById("lilengua").innerHTML;
-    document.getElementById("lilengua").innerHTML = liLengua + document.getElementById("lengua").value;
-    var liEfsi = document.getElementById("liefsi").innerHTML;
-    document.getElementById("liefsi").innerHTML = liEfsi + document.getElementById("efsi").value;
+    
+    document.getElementById("notamatematica").innerHTML = document.getElementById("matematica").value;
+    
+    document.getElementById("notalengua").innerHTML = document.getElementById("lengua").value;
+    
+    document.getElementById("notaefsi").innerHTML = document.getElementById("efsi").value;
 }
 
 function sacarPromedio() {
-    if(validarForms()){
+    if (validarForms()) {
         var total = 0;
         var div = document.getElementById("resultado");
         var img = document.getElementById("img");
         for (let index = 0; index < document.getElementsByTagName("input").length; index++) {
             total += Number(document.getElementsByTagName("input")[index].value);
         }
-        var promedio = total/document.getElementsByTagName("input").length;
+        var promedio = total / document.getElementsByTagName("input").length;
         if (promedio >= 6) {
             div.style.color = "lightgreen";
             img.setAttribute("src", "imgs/monkey-success.gif");
@@ -51,12 +51,21 @@ function sacarPromedio() {
 }
 
 function sacarMayorNota() {
-    if(validarForms()) {
+    if (validarForms()) {
         var formArray = document.getElementsByTagName("input");
         var notaMax = 0;
         for (let index = 0; index < formArray.length; index++) {
             if (formArray[index].value > notaMax) notaMax = formArray[index].value;
         }
-        
+        imprimirNotas();
+        if (document.getElementById("matematica").value == notaMax) {
+            document.getElementById("limatematica").style.color = "blue";
+        }
+        if (document.getElementById("lengua").value == notaMax) {
+            document.getElementById("lilengua").style.color = "blue";
+        }
+        if (document.getElementById("efsi").value == notaMax) {
+            document.getElementById("liefsi").style.color = "blue";
+        }
     }
 }
